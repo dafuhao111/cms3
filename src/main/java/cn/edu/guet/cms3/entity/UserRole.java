c187faffd9934564b5e3650f50af7f8b721944d4
liubinglin <2624560037@qq.com>
2026-06-30 11:00:36 +0800

    bushu

--- src/main/java/cn/edu/guet/cms3/entity/UserRole.java
+++ src/main/java/cn/edu/guet/cms3/entity/UserRole.java
@@ -1,11 +1,14 @@
 package cn.edu.guet.cms3.entity;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
 @TableName("user_role")
 public class UserRole {
+    @TableId
+    private Long id; // 新增主键
     private Long userId;
     private Integer roleId;
 }
